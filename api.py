1649a775e0ba7f8290791bdd39b26fb4f95ac32a
luizcdprof <luizcdprof@gmail.com>
2024-09-23 12:08:46 -0300

    adiciona apis externas para pesquisa

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
@@ -15,8 +16,32 @@ with app.app_context():
 
 @app.route('/api/movies', methods=['GET'])
 def get_movies():
-    movies = Movie.query.all()
-    return jsonify([{"id":movie.id,"title":movie.title,"genre":movie.genre} for movie in movies])
+    query_source = request.args.get('source')
+    query_movie = request.args.get('movie')
+    if not query_source or not query_movie:
+        movies = Movie.query.all()
+        return jsonify([{"id":movie.id,"title":movie.title,"genre":movie.genre} for movie in movies])
+    else:
+        if(query_source == 'omdb'):
+            query_type = request.args.get('type')
+            url = f'https://www.omdbapi.com/?apikey=d4c6caa6&t={query_movie}&type={query_type}'
+            headers = {
+                "accept": "application/json"
+            }
+        elif query_source == 'tmdb':
+            url = f'https://api.themoviedb.org/3/search/movie?query={query_movie}&include_adult=false&language=en-US&page=1'
+            headers = {
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmM0YTUzZjVlMTYyODY1ZDc4MWZkMDY2NDQyOGU5ZSIsIm5iZiI6MTcyNzA5NDEzMy4xNTkzNzEsInN1YiI6IjY2ZTgxZGQwMDUwZjE0ZTRmY2NmZjVjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.In1HXLOyBCx4rm0rrjQCVLWHZ6Bmwua_jh0OyOQ1Mh0"
+            }
+        else:
+            return jsonify({"error":"Fonte de pesquisa inválida"})
+        
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return jsonify(response.json()), 200
+        else:
+            return jsonify({"error":"Falha ao buscar dados da API externa! Verifique sua conexão com a internet e tente novamente."}), 500
 
 @app.route('/api/movies/<int:id>', methods=['GET'])
 def get_movie(id):
